fa55721c987f61bfcb1372982fe8c638f3179426
James Sullivan <81947235+jameswsullivan@users.noreply.github.com>
2024-02-29 19:01:59 -0600

    Add mysql commands

--- mysql.sql
+++ mysql.sql
@@ -1,19 +1,27 @@
+mysql -h host -u username -p
+
 -- Connect to MySQL server with auto-rehash disabled to save startup time.
 mysql --disable-auto-rehash --local-infile=1 -u root -p
 
-mysql -h host -u username -p
+SHOW DATABASES;
+
+SHOW TABLES;
 
 CREATE DATABASE <database-name>;
 
 DROP DATABASE <database-name>;
 
-CREATE USER 'username'@'host' IDENTIFIED BY 'password';
+CREATE USER `username`@`host` IDENTIFIED BY `password`;
 
-GRANT ALL PRIVILEGES ON <database-name>.* TO 'username'@'host';
+GRANT ALL PRIVILEGES ON <database-name>.* TO `username`@`host` WITH GRANT OPTION;
 
-REVOKE ALL PRIVILEGES ON <database-name>.* FROM 'username'@'host';
+FLUSH PRIVILEGES;
 
-SET PASSWORD FOR 'username'@'host' = 'password';
+REVOKE ALL PRIVILEGES ON <database-name>.* FROM `username`@`host`;
+
+SHOW GRANTS FOR `username`@`host`;
+
+SET PASSWORD FOR `username`@`host` = `password`;
 
 CREATE TABLE <table-name> (
     <col-name-01> INT,
@@ -28,7 +36,7 @@ CREATE TABLE <table-name> (
     <col-name-10> TIMESTAMP,
     <col-name-11> BOOLEAN,
     <col-name-12> BLOB,
-    <col-name-13> ENUM('value1', 'value2', 'value3')
+    <col-name-13> ENUM(`value1`, `value2`, `value3`)
 );
 
 INSERT INTO <table-name> (col1, col2, ...) VALUES (val1, val2, ...);
