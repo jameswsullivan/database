61f21dac647d404c4fa8ce121d7cc05de0d78b5c
jameswsullivan <81947235+jameswsullivan@users.noreply.github.com>
2024-04-29 11:18:25 -0500

    Update postgres notes

--- postgresql.sql
+++ postgresql.sql
@@ -8,78 +8,73 @@
 \c
 \q
 
--- Connection:
-psql -U username -h host -p port -W -d database_name
-pg_dump -U username -h host -p port -d database_name -f output_file.sql
+-- Making database connections:
+psql -U <USERNAME> -h <DATABASE_HOST> -p <PORT> -W -d <DATABASE_NAME>
+pg_dump -U <USERNAME> -h <DATABASE_HOST> -p <PORT> -d <DATABASE_NAME> -f OUTPUT_FILE.sql
 
 -- Create/drop database/roles.
-CREATE ROLE role_name WITH LOGIN SUPERUSER PASSWORD 'password' CONNECTION LIMIT -1;
-CREATE ROLE role_name WITH LOGIN PASSWORD 'password' CONNECTION LIMIT -1;
-CREATE DATABASE database_name WITH OWNER = role_name ENCODING = 'UTF8' CONNECTION LIMIT = -1;
-DROP DATABASE database_name;
+CREATE ROLE <ROLE_NAME> WITH LOGIN SUPERUSER PASSWORD '<PASSWORD>' CONNECTION LIMIT -1;
+CREATE ROLE <ROLE_NAME> WITH LOGIN PASSWORD '<PASSWORD>' CONNECTION LIMIT -1;
+CREATE DATABASE <DATABASE_NAME> WITH OWNER = <ROLE_NAME> ENCODING = 'UTF8' CONNECTION LIMIT = -1;
+DROP DATABASE <DATABASE_NAME>;
 
 -- Import/export a database:
+psql <DATABASE_NAME> < dump_file.sql
+pg_dump <DATABASE_NAME> > dump_file.sql
 
-psql database_name < dump_file.sql
-pg_dump database_name > dump_file.sql
 
--- Find postgres config files and max connections allowed.
-SHOW config_file;
-SHOW hba_file;
-pg_ctl reload
-SHOW MAX_CONNECTIONS;
-
--- Alter roles and privileges:
-ALTER ROLE user_name RENAME TO new_user_name;
-ALTER USER user_name WITH PASSWORD 'password';
+-- View and alter roles and privileges:
+ALTER ROLE <USERNAME> RENAME TO <NEW_USERNAME>;
+ALTER USER <USERNAME> WITH PASSWORD '<PASSWORD>';
 ALTER TABLE table_name OWNER TO new_owner;
 
-GRANT ALL PRIVILEGES ON DATABASE database_name TO user;
-GRANT CONNECT ON DATABASE database_name TO role_name;
-GRANT CONNECT ON DATABASE mydb TO user1, user2;
-GRANT USAGE ON SCHEMA public TO user1, user2;
-GRANT SELECT ON TABLE tablename TO user1, user2;
-GRANT INSERT, UPDATE, DELETE ON TABLE tablename TO user1;
+GRANT ALL PRIVILEGES ON DATABASE <DATABASE_NAME> TO <USER>;
+GRANT CONNECT ON DATABASE <DATABASE_NAME> TO <ROLE_NAME>;
+GRANT CONNECT ON DATABASE <DATABASE_NAME> TO <USER1>, <USER2>;
+GRANT USAGE ON SCHEMA public TO <USER1>, <USER2>;
+GRANT SELECT ON TABLE <TABLE_NAME> TO <USER1>, <USER2>;
+GRANT INSERT, UPDATE, DELETE ON TABLE <TABLE_NAME> TO <USER>;
 
-REVOKE CONNECT ON DATABASE mydb FROM user1, user2;
-REVOKE USAGE ON SCHEMA public FROM user1, user2;
-REVOKE SELECT ON TABLE tablename FROM user1, user2;
-REVOKE INSERT, UPDATE, DELETE ON TABLE tablename FROM user1;
-REVOKE CONNECT ON DATABASE your_database FROM PUBLIC;
+REVOKE CONNECT ON DATABASE <DATABASE_NAME> FROM <USER1>, <USER2>;
+REVOKE USAGE ON SCHEMA public FROM <USER1>, <USER2>;
+REVOKE SELECT ON TABLE <TABLE_NAME> FROM <USER1>, <USER2>;
+REVOKE INSERT, UPDATE, DELETE ON TABLE <TABLE_NAME> FROM <USER>;
+REVOKE CONNECT ON DATABASE <DATABASE_NAME> FROM PUBLIC;
 
+SELECT * FROM information_schema.columns WHERE table_name = '<TABLE_NAME>';
+
+-- Reference:
 -- Tc and CTC = ALL PRIVILEGES
 -- c = CONNECT
 -- https://www.postgresql.org/docs/current/ddl-priv.html
 -- https://docs.digitalocean.com/products/databases/postgresql/how-to/modify-user-privileges/
 
-SELECT * FROM information_schema.columns WHERE table_name = 'your_table_name';
-
 -- Show privileges for a specific user on databases:
 SELECT datname, has_database_privilege(grantee, datname, privilege_type) AS privilege
 FROM pg_database
 CROSS JOIN pg_roles
-WHERE rolname = 'your_username';
+WHERE rolname = '<ROLE_NAME>';
 
 -- Show privileges for a specific user on schemas:
 SELECT schema_name, has_schema_privilege(grantee, schema_name, privilege_type) AS privilege
 FROM information_schema.schemata
 CROSS JOIN pg_roles
-WHERE rolname = 'your_username';
+WHERE rolname = '<ROLE_NAME>';
 
 -- Show privileges for a specific user on tables:
 SELECT table_schema, table_name, privilege_type
 FROM information_schema.table_privileges
-WHERE grantee = 'your_username';
+WHERE grantee = '<ROLE_NAME>';
 
 -- Show privileges for a specific user on columns:
 SELECT table_schema, table_name, column_name, privilege_type
 FROM information_schema.column_privileges
-WHERE grantee = 'your_username';
+WHERE grantee = '<ROLE_NAME>';
 
 -- Checking for Data in a Specific Schema:
 SELECT schemaname, tablename, reltuples
 FROM pg_stat_user_tables
-WHERE schemaname = 'your_schema';
+WHERE schemaname = '<SCHEMA_NAME>';
 
 -- Checking for Data in the Entire Database:
 SELECT schemaname, tablename, reltuples
@@ -89,13 +84,8 @@ FROM pg_stat_all_tables;
 SELECT schema_name
 FROM information_schema.schemata;
 
--- pgcrypto extension:
-CREATE EXTENSION pgcrypto;
-SELECT * FROM pg_extension;
-SELECT * FROM pg_extension WHERE extname = 'pgcrypto';
 
 -- Find and disable OIDS:
-
 -- Find tables that have OIDS
 SELECT
     nspname,
@@ -115,13 +105,35 @@ WHERE nspname = 'public' AND relhasoids = true;
 ALTER TABLE <tablename> SET WITHOUT OIDS;
 ALTER TABLE <tablename> SET WITH OIDS;
 
+
+-- Create pgcrypto extension:
+CREATE EXTENSION pgcrypto;
+SELECT * FROM pg_extension;
+SELECT * FROM pg_extension WHERE extname = 'pgcrypto';
+
 -- View active/idle database connections:
-SELECT * FROM pg_stat_activity WHERE state = 'idle' AND datname = '<database_name>';
-SELECT * FROM pg_stat_activity WHERE state = 'active' AND datname = '<database_name>';
+SELECT * FROM pg_stat_activity WHERE state = 'idle' AND datname = '<DATABASE_NAME>';
+SELECT * FROM pg_stat_activity WHERE state = 'active' AND datname = '<DATABASE_NAME>';
 
 SELECT
     datname,
     client_hostname,
     query 
 FROM pg_stat_activity 
-WHERE state = 'idle' AND datname = '<database_name>';
+WHERE state = 'idle' AND datname = '<DATABASE_NAME>';
+
+-- Rename databases:
+ALTER DATABASE <old_db_name> RENAME TO <new_db_name>;
+
+-- Terminate connections to a database from the server side:
+SELECT  pg_terminate_backend(pid)
+FROM  pg_stat_activity
+WHERE
+    datname = '<TARGET_DB_NAME>'
+    AND leader_pid IS NULL;
+
+-- Find postgres config files and max connections allowed.
+SHOW config_file;
+SHOW hba_file;
+pg_ctl reload
+SHOW MAX_CONNECTIONS;
